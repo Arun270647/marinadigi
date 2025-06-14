3221cd8ef30e5443aa2cfd864672199c1982eb92
itsrahul_8 <149181581+rahuldk1105@users.noreply.github.com>
2025-06-15 01:16:52 +0530

    changed the about page , optimized for mobile view also

--- src/Pages/About/Index.tsx
+++ src/Pages/About/Index.tsx
@@ -1,8 +1,5 @@
-import SkillsSection from "../../Components/About/SkillsSection";
 import WhyChooseUs from "../../Components/About/WhyChooseUs";
-import CompanySliderSection from "../../Components/Common/CompanySliderSection";
 import CtaSection from "../../Components/Common/CtaSection";
-import PageHero from "../../Components/Common/PageHero";
 import ServiceSection2 from "../../Components/Common/ServiceSection2";
 import AboutSection from "../../Pages/About/AboutSection";
 import Footer from "../../Layouts/Footer3";
@@ -19,16 +16,8 @@ const About = () => {
     <>
       <Header />
       <MobileHeaderSidebar />
-      <PageHero
-        className="about-header-area"
-        title={"About Us"}
-        breadcrumb={["Home", "About Us"]}
-      />
-      <CompanySliderSection className="slider-section-area slider-inner sp5" />
       <AboutSection />
       <WhyChooseUs />
-      {/* <HistorySection /> */}
-      <SkillsSection />
       <ServiceSection2 className="service2-section-area sp1 bg2" />
       <CtaSection />
       <Footer />
