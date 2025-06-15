521ee4980fa54751a8ef9bd58c1df1d286e4d45c
itsrahul_8 <149181581+rahuldk1105@users.noreply.github.com>
2025-06-15 08:43:47 +0530

    all pages changed except seo and about page , yabba saamy mudiyala daa arun ehh

--- src/Pages/service5/index.tsx
+++ src/Pages/service5/index.tsx
@@ -3,9 +3,7 @@ import PageHero from "../../Components/Common/PageHero";
 import ChooseSection from "../../Components/Service2/ChooseSection";
 import Footer from "../../Layouts/Footer3";
 import Header from "../../Layouts/Header6";
-import SliderSection from "../../Components/Service2/SliderSection";
 import ServiceInner5Header from "../../Components/Service5/ServiceInnerHeader";
-// import BenefitsSection from "../../Components/Service3/BenefitsSection";
 import ServiceSection from "../../Components/Service5/ServiceSection";
 import { useEffect } from "react";
 
@@ -25,11 +23,9 @@ const Service5Page = () => {
             <PageHero
                 className="about-header-area"
                 title={"Social Media Marketing"}
-                breadcrumb={["Home", "Services", "Social Media Marketing"]}
+                breadcrumb={[""]}
             />
             <ServiceInner5Header/>
-            <SliderSection className={'slider-section-area slider-inner sp5'} />
-            {/* <BenefitsSection/> */}
             <ServiceSection/>
             <ChooseSection/>
             <CtaSection />
